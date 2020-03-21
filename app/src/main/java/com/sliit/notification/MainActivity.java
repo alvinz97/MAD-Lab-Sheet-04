5bb9e03126af0d59a6ce480a776756d7019d0779
[alvinz97] <it18211306@my.sliit.lk>
2020-03-21 18:21:04 +0530

    Implement the function to send Notification to open new Activity - Notifications Update 1.0

--- app/src/main/java/com/sliit/notification/MainActivity.java
+++ app/src/main/java/com/sliit/notification/MainActivity.java
@@ -1,9 +1,12 @@
 package com.sliit.notification;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -35,9 +38,23 @@ public class MainActivity extends AppCompatActivity {
         sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SignUp.class);
-                startActivity(intent);
+                sendNotification();
             }
         });
+    }
+    public void sendNotification() {
+        Intent getIntent = getIntent();
+
+        Intent intent = new Intent(this, SignUp.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("My notification")
+                .setContentText("getName")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent).setAutoCancel(true);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(0,builder.build());
     }
 }
